a476b16d1ea0ef9af5ad33e54731feb5d9b3be31
Matt Swain <m.swain@me.com>
2014-07-18 15:20:54 +0100

    Mock RDKit so autodoc works

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -262,3 +262,30 @@ def process_docstring(app, what, name, obj, options, lines):
 
 def setup(app):
     app.connect('autodoc-process-docstring', process_docstring)
+
+
+class Mock(object):
+    """Mock."""
+
+    __all__ = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            mockType = type(name, (), {})
+            mockType.__module__ = __name__
+            return mockType
+        else:
+            return Mock()
+
+MOCK_MODULES = ['rdkit']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
