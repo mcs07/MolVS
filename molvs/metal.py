c96f4e1f388905b2970cba33c0af717fe3085ac9
Matt Swain <m.swain@me.com>
2016-12-01 23:01:09 +0000

    Fix metal disconnection bug

--- molvs/metal.py
+++ molvs/metal.py
@@ -29,8 +29,6 @@ class MetalDisconnector(object):
         # TODO: Use atomic numbers instead of element symbols in SMARTS to allow for isotopes?
         self._metal_nof = Chem.MolFromSmarts('[Li,Na,K,Rb,Cs,F,Be,Mg,Ca,Sr,Ba,Ra,Sc,Ti,V,Cr,Mn,Fe,Co,Ni,Cu,Zn,Al,Ga,Y,Zr,Nb,Mo,Tc,Ru,Rh,Pd,Ag,Cd,In,Sn,Hf,Ta,W,Re,Os,Ir,Pt,Au,Hg,Tl,Pb,Bi]~[N,O,F]')
         self._metal_non = Chem.MolFromSmarts('[Al,Sc,Ti,V,Cr,Mn,Fe,Co,Ni,Cu,Zn,Y,Zr,Nb,Mo,Tc,Ru,Rh,Pd,Ag,Cd,Hf,Ta,W,Re,Os,Ir,Pt,Au]~[B,C,Si,P,As,Sb,S,Se,Te,Cl,Br,I,At]')
-        self._free_metal = Chem.MolFromSmarts('[Li,Na,K,Mg,CaX0+0]')
-        self._carboxylic = Chem.MolFromSmarts('[CX3](=O)[OX2H1]')
 
     def __call__(self, mol):
         """Calling a MetalDisconnector instance like a function is the same as calling its disconnect(mol) method."""
@@ -51,18 +49,17 @@ class MetalDisconnector(object):
         :rtype: :rdkit:`Mol <Chem.rdchem.Mol-class.html>`
         """
         log.debug('Running MetalDisconnector')
-        # TODO: In future, maybe insert zero-order complex/ionic/dative bonds instead of disconnecting?
         # Remove bonds that match SMARTS
         for smarts in [self._metal_nof, self._metal_non]:
             pairs = mol.GetSubstructMatches(smarts)
-            edmol = Chem.EditableMol(mol)
+            rwmol = Chem.RWMol(mol)
             orders = []
             for i, j in pairs:
                 # TODO: Could get the valence contributions of the bond instead of GetBondTypeAsDouble?
                 orders.append(int(mol.GetBondBetweenAtoms(i, j).GetBondTypeAsDouble()))
-                edmol.RemoveBond(i, j)
+                rwmol.RemoveBond(i, j)
             # Adjust neighbouring charges accordingly
-            mol = edmol.GetMol()
+            mol = rwmol.GetMol()
             for n, (i, j) in enumerate(pairs):
                 chg = orders[n]
                 atom1 = mol.GetAtomWithIdx(i)
@@ -70,18 +67,5 @@ class MetalDisconnector(object):
                 atom2 = mol.GetAtomWithIdx(j)
                 atom2.SetFormalCharge(atom2.GetFormalCharge() - chg)
                 log.info('Removed covalent bond between %s and %s', atom1.GetSymbol(), atom2.GetSymbol())
-        # Ionize a free neutral metal with a protonated carboxylic acid
-        # TODO: Extend this to other acids?
-        # TODO: Move to charge module?
-        fms = mol.GetSubstructMatches(self._free_metal)
-        carbs = mol.GetSubstructMatches(self._carboxylic)
-        if len(fms) == len(carbs) > 0:
-            for fm in fms:
-                atom = mol.GetAtomWithIdx(fm[0])
-                atom.SetFormalCharge(atom.GetFormalCharge() + 1)
-            for carb in carbs:
-                atom = mol.GetAtomWithIdx(carb[2])
-                atom.SetFormalCharge(atom.GetFormalCharge() - 1)
-        log.debug(Chem.MolToSmiles(mol))
         Chem.SanitizeMol(mol)
         return mol
---
+++ tests/test_metal.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Tests for metal.py"""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+import logging
+
+from rdkit import Chem
+
+from molvs.metal import MetalDisconnector
+from molvs.standardize import standardize_smiles
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def test_standardize():
+    """Test covalent metal is disconnected during standardize."""
+    assert standardize_smiles('CCC(=O)O[Na]') == 'CCC(=O)[O-].[Na+]'
+
+
+def test_standardize2():
+    """Test metal ion is untouched during standardize."""
+    assert standardize_smiles('CCC(=O)[O-].[Na+]') == 'CCC(=O)[O-].[Na+]'
+
+
+def test_standardize3():
+    """Test Hg is disconnected from O during standardize."""
+    assert standardize_smiles('CCC(=O)O[Hg]') == 'CCC(=O)[O-].[Hg+]'
+
+
+def test_standardize4():
+    """Test dimethylmercury is not disconnected during standardize."""
+    assert standardize_smiles('C[Hg]C') == 'C[Hg]C'
+
+
+def test_metaldisconnector1():
+    """Test direct usage of MetalDisconnector class."""
+    mol = Chem.MolFromSmiles('NC(CC(=O)O)C(=O)[O-].O.O.[Na+]')
+    md = MetalDisconnector()
+    mol = md.disconnect(mol)
+    assert Chem.MolToSmiles(mol) == 'NC(CC(=O)O)C(=O)[O-].O.O.[Na+]'
+
+
+def test_metaldisconnector2():
+    """Test direct usage of MetalDisconnector class."""
+    mol = Chem.MolFromSmiles('CCC(=O)O[Na]')
+    md = MetalDisconnector()
+    mol = md.disconnect(mol)
+    assert Chem.MolToSmiles(mol) == 'CCC(=O)[O-].[Na+]'
+
+
+
+
